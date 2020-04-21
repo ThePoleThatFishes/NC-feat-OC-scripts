7d63386c4455b3961ba8f02482f4ecdd9cd0bbdb
ThePoleThatFishes <63863162+ThePoleThatFishes@users.noreply.github.com>
2020-04-21 14:25:02 +0300

    Added v3 fission, v2 fusion
    Scripts can now be controlled via the keyboard

---
+++ nc_fission_v3.lua
@@ -0,0 +1,124 @@
+local c = require("component")
+local t = require("term")
+local event = require("event")
+local fisr = c.nc_fission_reactor
+
+print("Welcome to Fission Controller v3 - by ThePoleThatFishes")
+print("Hold Q to quit, U to force an update. (If stats are looking wrong!)")
+os.sleep(1)
+print("This message will self destruct in 5 seconds.")
+print("After the self-destruction, the script will begin.")
+os.sleep(5)
+
+local x = fisr.getLengthX()
+local y = fisr.getLengthY()
+local z = fisr.getLengthZ()
+local size = string.format("%s*%s*%s", x, y, z)
+local max_rf = fisr.getMaxEnergyStored()
+local max_hu = fisr.getMaxHeatLevel()
+local mult_rf = fisr.getEfficiency() .. "%"
+local mult_hu = fisr.getHeatMultiplier() .. "%"
+local no_cells = fisr.getNumberOfCells()
+local k = 1
+
+
+local function fuelStats()
+	fuel = fisr.getFissionFuelName()
+	fuel_base = string.format("%s RF/t - %s HU/t", fisr.getFissionFuelPower(), fisr.getFissionFuelHeat())
+	rf_t = fisr.getReactorProcessPower() .. " RF/t"
+	net_hu = fisr.getReactorProcessHeat() .. " HU/t"
+	return fuel, fuel_rf_t, fuel_hu_t, rf_t, net_hu
+end
+
+local function currentProcess()
+	rf = fisr.getEnergyStored()
+	hu = fisr.getHeatLevel()
+	rf_lvl = string.format("%s/%s RF", rf, max_rf)
+	hu_lvl = string.format("%s/%s HU", hu, max_hu)
+	timeleft = (fisr.getReactorProcessTime() - fisr.getCurrentProcessTime()/no_cells)/20
+	return rf, hu, timeleft
+end
+
+local function reactorControl()
+	if rf/max_rf > 0.4 or hu/max_hu > 0.2 then
+		fisr.deactivate()
+	else
+		fisr.activate()
+	end
+end
+
+local function reactorStatus()
+	if fisr.isProcessing() then
+		stat = "Active"
+	else
+		if hu/max_hu > 0.2 then
+			stat = "Inactive - Cooling"
+		elseif rf/max_rf > 0.4 then
+			stat = "Inactive - Discharging"
+		elseif fuel == "No Fuel" then
+			stat = "Inactive - No Fuel"
+		else
+			stat = "Inactive - " .. fisr.getProblem()
+		end
+	end
+	return stat
+end
+
+local function infoOutput()
+	fisrStatsNames = {"Size: ", "Cells: ", "Eff. Mult.: ", "Heat Mult.: ", "Energy: ", "Heat Level: ", "Status: "}
+	fisrStats = {size, no_cells, mult_rf, mult_hu, rf_lvl, hu_lvl, stat}
+	fuelInfoNames = {"Fuel: ", "Base Power-Heat: ", "Energy Gen: ", "Net Heat: ", "Time Left: "}
+	fuelInfo = {fuel, fuel_base, rf_t, net_hu, string.format("%.1f sec", timeleft)}
+end
+
+function fisrInit()
+	fuelStats()
+	currentProcess()
+	reactorControl()
+	reactorStatus()
+	infoOutput()
+	print("Reactor Info")
+	for i = 1, 7 do
+		print(fisrStatsNames[i] .. fisrStats[i])
+	end
+	print("Fuel Info")
+	for i = 1, 5
+		print(fuelInfoNames[i] .. fuelInfo[i])	
+	end
+end
+
+function fisrRun()
+	while k > 0 do
+		local ev, ad, chr, code, player = event.pull(0.35)
+		if ev == "key_down" and chr == 113 then
+			print("Quitting...")
+			break
+		end
+		if ev == "key_down" and chr == 117 then
+			t.clear()
+			fisrInit()
+		end
+		currentProcess()
+		reactorControl()
+		reactorStatus()
+		infoOutput()
+		local clearLines = {6, 7, 8, 14}
+		for i = 1, 4 do
+			t.setCursor(1, clearLines[i])
+			t.clearLine()
+			if clearLines[i] ~= 14 then
+				print(fisrStatsNames[i+4] .. fisrStats[i+4])
+			else
+				print(fuelInfoNames[i+1] .. fuelInfo[i+1])
+			end
+		end
+		os.sleep(0.35)
+	end
+end
+
+t.clear()
+fisrInit()
+fisrRun()
+		
+		
+	
--- nc_fusion.lua
+++ nc_fusion.lua
@@ -1,14 +1,22 @@
-c = require("component")
-t = require("term")
-fusr = c.nc_fusion_reactor
+local c = require("component")
+local t = require("term")
+local event = require("event")
+local fusr = c.nc_fusion_reactor
+
+print("Welcome to Fusion Controller v2 by ThePoleThatFishes!")
+print("Hold down Q to quit, U to force updates. (If stats are looking wrong!")
+print("This message will self destruct in 5 seconds.")
+os.sleep(5)
 t.clear()
 
-local size = fusr.getToroidSize()
-local fuel1 = fusr.getFirstFusionFuel()
-local fuel2 = fusr.getSecondFusionFuel()
-local bestMK = fusr.getFusionComboHeatVariable()*1.21875567483
-local combo_rf = fusr.getFusionComboPower()*100
-local combo_time = fusr.getFusionComboTime()
+local function fusrStats()
+	size = fusr.getToroidSize()
+	fuel1 = fusr.getFirstFusionFuel()
+	fuel2 = fusr.getSecondFusionFuel()
+	bestMK = fusr.getFusionComboHeatVariable()*1.21875567483
+	combo_rf = fusr.getFusionComboPower()*100
+	combo_time = fusr.getFusionComboTime()
+end
 
 function stats()
 	rf_t = fusr.getReactorProcessPower()
@@ -17,22 +25,37 @@ function stats()
 	if fusr.isProcessing() then
 		stat = "Active"
 	elseif (1 - tempMK/bestMK) < 0.002 then
-		stat = "Inactive - Cooling Down"
-	else
-		stat = fusr.getProblem()
+		stat = "Inactive - Cooling"
+	elseif tempMK < 8 then
+		stat = "Inactive - Has not ignited"
+	elseif fuel1 == "Empty" or fuel2 == "Empty" then
+		stat = "Inactive - Out of fuel"
 	end
-	fusrINames = {"Size: ", "Fuel Combo: ", "Combo RF/t - Lifetime: ", "Temp/Optimal Temp: ", "Energy Gen: ", "Efficiency: ", "Status: "}
-	fusrI = {size, string.format("%s/%s", fuel1, fuel2), string.format("%s RF/t - %s t", combo_rf, combo_time), string.format("%s MK/%s MK", tempMK, bestMK),
-		string.format("%.1f RF/t", rf_t), string.format("%.3f", eff), stat}
+	fusrINames = {"Toroid Size: ", "Fuel Combo: ", "Combo RF/t - Lifetime: ", "Temp/Optimal Temp: ", "Energy Gen: ", "Efficiency: ", "Status: "}
+	fusrI = {size, string.format("%s/%s", fuel1, fuel2), string.format("%s RF/t - %s t", combo_rf, combo_time),
+				string.format("%.1f MK/%.1f MK", tempMK, bestMK), string.format("%.1f RF/t", rf_t), string.format("%.3f", eff), stat}
 end
 
+fusrStats()
 stats()
 for i = 1, 3 do
 	print(fusrINames[i] .. fusrI[i])
 end
 
-timer = 1200 --placeholder condition, will replace once keyboard events are a thing!
-while timer > 0 do
+k = 1
+while k > 0 do
+	local ev, ad, chr, code, player = event.pull(0.5)
+	if ev == "key_down" and chr == 113 then
+		print("Quitting...")
+		break
+	end
+	if ev == "key_down" and chr = 117 then
+		fusrStats()
+		t.clear()
+		for i = 1,7 do
+			print(fusrINames[i] .. fusrI[i])
+		end
+	end
 	stats()
 	if (1 - tempMK/bestMK) < 0.002 then
 		fusr.deactivate()
@@ -45,6 +68,8 @@ while timer > 0 do
 		print(fusrINames[i] .. fusrI[i])
 	end
 	os.sleep(0.5)
-	timer = timer - 1
 end
 
+		
+
+	
